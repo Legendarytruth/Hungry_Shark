c62268c0431226d8d1299d60f21f6b271cb5e786
Legendarytruth <arthur.sit99@gmail.com>
2021-10-14 17:26:51 -0400

    Better Finish

--- Assets/FishScript.cs
+++ Assets/FishScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 public class FishScript : MonoBehaviour
 {
     private GameHandler gameHandler;
+    private Vector3 moveDir;
     public int points = 5;
     public float speed;
     public bool damage = false;
@@ -12,12 +13,16 @@ public class FishScript : MonoBehaviour
     public Vector3 size;
     private readonly float changeDirTime = 5f;
     private float recentDirChange;
+    public float turnSmooth = 0.1f;
+    float turnVelocity;
+
+    public NavMeshAgent nav;
     // Start is called before the first frame update
     public void Start()
     {
         gameHandler = GameHandler.Instance;
         playerStats = GameObject.FindGameObjectWithTag("PlayerStats").GetComponent<PlayerStats>();
-        //moveFish();
+        nav = gameObject.GetComponent<NavMeshAgent>();
     }
 
     public void setSize(Vector3 num)
@@ -62,5 +67,25 @@ public class FishScript : MonoBehaviour
             
             Destroy(gameObject);
         }
+    }
+
+    private void moveFish()
+    {
+        if (Time.time + recentDirChange > changeDirTime)
+        {
+            recentDirChange = Time.time;
+            Vector3 dir = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmooth);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * speed * Time.deltaTime;
+        }
+        nav.Move(moveDir);
+    }
+    private void Update()
+    {
+        moveFish();
     }
 }
